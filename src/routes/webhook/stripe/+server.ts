72f64abea006122073a1899f672c7be481af20f8
Daisho Komiyama <daishokomiyama@gmail.com>
2025-10-04 20:48:49 -0400

    Response immediately, do my work asynchronously

--- src/routes/webhook/stripe/+server.ts
+++ src/routes/webhook/stripe/+server.ts
@@ -3,34 +3,49 @@ import { fullFillPayment, stripe } from '$lib/server/utils.js';
 import { error } from '@sveltejs/kit';
 
 export const POST = async ({ request }) => {
-    const signature = request.headers.get('stripe-signature')
+    console.log('==> START', new Date().toISOString());
+
+    const signature = request.headers.get('stripe-signature');
     if (!signature || !STRIPE_WEBHOOK_SECRET) {
         return error(400, 'Missing signature or webhook secret');
     }
 
-    const rawBody = await request.text()
+    const rawBody = await request.text();
 
+    let event;
     try {
-        const event = stripe.webhooks.constructEvent(
+        // 1. Respond immediately
+        event = stripe.webhooks.constructEvent(
             rawBody,
             signature,
             STRIPE_WEBHOOK_SECRET
         );
-
-        switch (event.type) {
-            case 'checkout.session.completed':
-            case 'checkout.session.async_payment_succeeded': {
-                const success = await fullFillPayment(event.data.object.id);
-                if (!success) return error(400, 'Failed to fulfill payment');
-                break;
-            }
-            default:
-                console.log(`Unhandled event type ${event.type}`);
-        }
-
-        return new Response(JSON.stringify({ received: true }), { status: 200 });
     } catch (err) {
         console.error('Error verifying webhook signature:', err);
-        throw error(400, 'Webhook Error: ' + (err as Error).message);
+        return error(400, 'Webhook Error: ' + (err as Error).message);
     }
+
+    // 2. Do your work asynchronously
+    queueMicrotask(async () => {
+        try {
+            switch (event.type) {
+                case 'checkout.session.completed':
+                case 'checkout.session.async_payment_succeeded': {
+                    const success = await fullFillPayment(event.data.object.id);
+                    if (!success) {
+                        console.error('Failed to fulfill payment for', event.id);
+                        // Stripe will already retry if fulfillment itself fails
+                    }
+                    break;
+                }
+                default:
+                    console.log(`Unhandled event type ${event.type}`);
+            }
+        } catch (err) {
+            console.error('Error handling event asynchronously:', err);
+        }
+    });
+
+    // 2. Stripe only cares about a 2xx
+    return new Response("ok", { status: 200 });
 };
